202169841efc062f2d8e841fd323d05a26d9db93
Harivansh-coder <harivanshthakur8@gmail.com>
2022-12-09 13:25:04 +0530

    done boom boom changes

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/sha512"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"log"
 	"os"
 )
+
+func generate_hash(file *os.File, algo string) string {
+
+	h := sha1.New()
+
+	switch algo {
+	case "SHA256":
+		h = sha256.New()
+	case "SHA512":
+		h = sha512.New()
+	}
+
+	if _, err := io.Copy(h, file); err != nil {
+		log.Fatal(err)
+	}
+
+	return hex.EncodeToString(h.Sum(nil))
+
+}
 
 func main() {
 	f, err := os.Open(os.Args[1])
@@ -14,17 +36,14 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	hash_string := generate_hash(f, os.Args[2])
+
 	defer f.Close()
 
-	h := sha256.New()
-	if _, err := io.Copy(h, f); err != nil {
-		log.Fatal(err)
-	}
-
-	if string(h.Sum(nil)) == os.Args[2] {
-		fmt.Println("good to go :)")
+	if hash_string == os.Args[3] {
+		fmt.Println("Good to go :)")
 	} else {
-		fmt.Println("You have been F ;)")
+		fmt.Println("You have been F*** ;)")
 	}
-
 }
